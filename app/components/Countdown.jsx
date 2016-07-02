951e3e62c7f43e2b968315b712e023d70d7e4857
Sang Yi <Sang Yi>
2016-07-01 19:55:13 -0500

    set status to stop when timer hits 0

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -27,12 +27,23 @@ var Countdown = React.createClass({
     }
   },
 
+  componentWillUnmount: function(){
+
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+
   startTimer: function(){
-    this.timer = setInterval(() => {
+
+      this.timer = setInterval(() => {
       var newCount = this.state. count - 1;
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+
+      if (newCount === 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
     },1000);
   },
 
@@ -50,6 +61,7 @@ var Countdown = React.createClass({
   },
 
   render: function(){
+
     var {count, countdownStatus} = this.state;
     var renderControlArea = () => {
       if (countdownStatus !== 'stopped') {
